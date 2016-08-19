9be67424f5415adad4b24f2c9555ce5413200bc9
표병식 <byungsik.pyo@hyundai-uni.com>
2016-08-19 17:05:20 +0900

    패키지명 변경

--- src/test/java/com/hyundaiuni/nxtims/controller/app/UsersControllerTests.java
+++ src/test/java/com/hyundaiuni/nxtims/controller/app/UsersControllerTests.java
@@ -1,4 +1,4 @@
-package com.hyundaiuni.nxtims.controller.framework;
+package com.hyundaiuni.nxtims.controller.app;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
