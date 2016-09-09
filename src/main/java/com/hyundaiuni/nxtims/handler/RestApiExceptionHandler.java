6b8c1047fe81b4a6e68608cde0e350a9b7db30d1
표병식 <byungsik.pyo@hyundai-uni.com>
2016-09-09 16:28:06 +0900

    Array.toList를 CollectionUtils.arrayToList로 변경

--- src/main/java/com/hyundaiuni/nxtims/handler/RestApiExceptionHandler.java
+++ src/main/java/com/hyundaiuni/nxtims/handler/RestApiExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.hyundaiuni.nxtims.handler;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -30,7 +30,7 @@ public class RestApiExceptionHandler {
 
         error.put("CODE", e.getCode());
         error.put("MESSAGE", e.getMessage());
-        error.put("ARGUMENTS", StringUtils.collectionToDelimitedString(Arrays.asList(e.getArgs()), "^"));
+        error.put("ARGUMENTS", StringUtils.collectionToDelimitedString(CollectionUtils.arrayToList(e.getArgs()), "^"));
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
